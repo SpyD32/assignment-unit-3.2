82b26b91ebbfea7db39ffd644677a415eca5fae1
Michael Silas <michael.silas30@gmail.com>
2024-08-13 09:19:15 -0500

    submit

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -1,47 +1,91 @@
-console.log('****** Loops Practice *******');
+// console.log('****** Loops Practice *******');
 
 
 // 1. 'for' loop
-console.log('---- 1. For loops ----');
+// console.log('---- 1. For loops ----');
+
+
+  
+  
+
 // Example: a for loop to console.log numbers from 0 to 3
-console.log('count from 0 to 3');
-// start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
-for (let i=0; i<4; i++) {  
-  console.log(i);
+// console.log('count from 0 to 3');
+for (let i = 0; i < 3; i++) {
+// console.log('count from 0 to 3');
+   
 }
+// start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
+
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
-console.log('count from 0 to 5');
+for (let i = 0; i<=5; i++) {
+  // console.log(i);
+//the expression is what needs to be changed. 
+  
+}
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
-console.log('count from 3 to 5');
+//for the example loop to be changed, it would be let i = 3; instead of
+
+for (let i = 3; i <=5; i++) {
+  //  console.log('count from 3 to 5');
+}
+
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
+for (let i = 2; i <=10; i++) {
+let even = [2, 4, 6, 8, 10];
+//coding after the condition you are able to add another veriable for even numbers. 
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
+  
+}
+// console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
-console.log('STRETCH: countdown from 5 to 0');
+for (let i = 5; i <=0; i++) {
+  // console.log();
+
+}
+// console.log('STRETCH: countdown from 5 to 0');
+for (let i = 5; i <=0; i++) {
+// console.log('countdown from 5 to 0');
+  
+}
+
 
 
 // 2. For of loops  
-console.log('---- 2. For Of loop ----');
+// console.log('---- 2. For Of loop ----');
 let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
+for (let char of stars) {
+  // console.log(char);
+  //looked up to see why char was used but didn't see an explanation. Might need better explanation to understand the concept. 
+}
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
-console.log('Some stars:');
+// console.log('Some stars:');
 
 
 // 3. While loops  
-console.log('---- 3. While loop ----');
+// console.log('---- 3. While loop ----');
+//as long as the condition is true, the while loop will keep going until it's not true. 
+//therefore if the statement continues to be true there will be an infinite loop. 
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
-console.log('Some stars using while:');
+// console.log('Some stars using while:');
+  while(stars)
+
+  
+
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
-console.log('count from 0 to 5');
+// console.log('count from 0 to 5');
+
+  
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
-console.log('count backwards from 10 to 5');
+// console.log('count backwards from 10 to 5');
